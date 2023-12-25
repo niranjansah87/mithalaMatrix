0d0632907fce0cf41b78a2c6a9727c09791164b7
Niranjan <2100090187@kluniversity.in>
2023-12-25 12:18:42 +0530

    docs implemenatation

--- apps/docs/pages/_error.js
+++ apps/docs/pages/_error.js
@@ -4,9 +4,11 @@ import * as Sentry from '@sentry/nextjs';
 
 const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   if (!hasGetInitialPropsRun && err) {
-   
+    // getInitialProps is not called in case of
+    // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
+    // err via _app.js so it can be captured
     Sentry.captureException(err);
-    
+    // Flushing is not required in this case as it only happens on the client
   }
 
   return <NextErrorComponent statusCode={statusCode} />;
@@ -17,10 +19,11 @@ MyError.getInitialProps = async (context) => {
   
   const { res, err, asPath } = context;
 
- 
+  // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
+  // getInitialProps has run
   errorInitialProps.hasGetInitialPropsRun = true;
 
- 
+  // Returning early because we don't want to log 404 errors to Sentry.
   if (res?.statusCode === 404) {
     return errorInitialProps;
   }
